4e4982a7e5dcb74acaf76f576c26505f87de8d43
minniesBaa <julianjohnsontaylor@gmail.com>
2025-01-20 17:08:43 -0800

    Initial release

---
+++ servertester2/servertester2/flag.vb
@@ -0,0 +1,25 @@
+﻿Imports System
+
+Public Module flag
+
+
+    Public Function getarg() As String
+
+        Dim args As String() = Environment.GetCommandLineArgs()
+
+
+        If args.Length > 1 Then
+
+            Dim firstFlag As String = args(1)
+
+            ' get flag 1
+            Console.WriteLine("First flag: " & firstFlag)
+            Return (firstFlag)
+        Else
+            Console.WriteLine("No flags provided.")
+            Return ("")
+        End If
+
+    End Function
+
+End Module
---
+++ servertester2/servertester2/tester.vb
@@ -0,0 +1,55 @@
+﻿Imports System.Drawing
+Imports System.Net.NetworkInformation
+Imports System.Windows.Forms
+
+Public Class tester
+
+    Private Shared notifyIcon As NotifyIcon
+
+    Public Shared Function IsServerOnline(ByVal serverName As String) As Boolean
+        Dim arg As String
+        arg = flag.getarg()
+        Try
+            Dim ping As New Ping
+            Dim reply As PingReply = ping.Send(serverName)
+
+
+
+            If reply.Status = IPStatus.Success Then
+                If arg = "-showOn" Then
+                    ShowToastNotification("Server Info", "The server is on.")
+                End If
+
+                Return True
+            Else
+                ShowToastNotification("Server Offline", "The server is offline.")
+                Return False
+            End If
+
+        Catch ex As Exception
+            ShowToastNotification("Server Error", "Error pinging server: " & ex.Message)
+            Return False
+        End Try
+    End Function
+
+    Public Shared Sub Main()
+
+        notifyIcon = New NotifyIcon()
+        notifyIcon.Icon = SystemIcons.Application
+        notifyIcon.Visible = True
+
+
+
+        Dim server As String = "server.ip.adress.here"
+        IsServerOnline(server)
+
+
+
+    End Sub
+
+    Private Shared Sub ShowToastNotification(ByVal title As String, ByVal message As String)
+        notifyIcon.BalloonTipTitle = title
+        notifyIcon.BalloonTipText = message
+        notifyIcon.ShowBalloonTip(5000) ' Show for 5 seconds
+    End Sub
+End Class
